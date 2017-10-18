291fe5fc8abc968554bda55996b5d4c3717f79e1
AndrewGM <andrewalangm@gmail.com>
2017-10-18 00:14:17 -0500

    Listado de tiendas

---
+++ lista_paginas.php
@@ -0,0 +1,40 @@
+<?php 
+	$tiendas = [
+		'1' => 'BestBuy',
+		'2' => 'Claro shop',
+		'3' => 'Costco'
+	];
+
+	$categorias = [
+		'40' => 'Accion & Videocámaras',
+		'35' => 'Audifonos',
+		'36' => 'Bocinas',
+		'52' => 'Bocina Bluetooth',
+		'53' => 'Bocina Wifi',
+		'54' => 'Soundbar',
+		'39' => 'Cámaras',
+		'81' => 'Cámaras Digitales Compactas',
+		'68' => 'Camera Reflex',
+		'58' => 'Disco Duro Externo',
+		'37' => 'Home Theater',
+		'29' => 'Lavadora',
+		'48' => 'Lentes de Cámera',
+		'51' => 'Laptop',
+		'33' => 'Licuadora',
+		'34' => 'Microondas',
+		'42' => 'Proyector',
+		'32' => 'Secadora',
+		'50' => 'Smartphone',
+		'47' => 'Smartwatches',
+		'46' => 'Tablets',
+		'30' => 'Televisor',
+		'1'=> 'Uncategorized',
+		'217' => 'VideoJuegos',
+		'218' => 'Equipos de Computo'
+	];
+
+	$paginas = [
+	];
+
+	echo json_encode($paginas);
+ ?>
