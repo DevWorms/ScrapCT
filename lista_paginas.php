d99f1889772dccc9303d22cfa9b621bc9eb80202
AndrewGM <andrewalangm@gmail.com>
2017-10-18 01:36:32 -0500

    Primeras tres tiendas

--- lista_paginas.php
+++ lista_paginas.php
@@ -34,6 +34,355 @@
 	];
 
 	$paginas = [
+		[
+			'tienda' => 1,
+			'categoria' => 40,
+			'url' => 'http://www.bestbuy.com.mx/c/videocamaras/c67'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 35,
+			'url' => 'http://www.bestbuy.com.mx/c/audifonos/c71'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 36,
+			'url' => 'http://www.bestbuy.com.mx/c/bocinas-portatiles/c72'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 52,
+			'url' => 'http://www.bestbuy.com.mx/c/bocinas-portatiles/c72'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 53,
+			'url' => 'http://www.bestbuy.com.mx/c/bocinas-inalambricas/c73'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 81,
+			'url' => 'http://www.bestbuy.com.mx/c/camaras-digitales/c65'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 68,
+			'url' => 'http://www.bestbuy.com.mx/c/camaras-profesionales/c66'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 58,
+			'url' => 'http://www.bestbuy.com.mx/c/discos-duros-y-almacenamiento/c44'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 37,
+			'url' => 'http://www.bestbuy.com.mx/c/sistemas-de-audio/c75'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 29,
+			'url' => 'http://www.bestbuy.com.mx/c/lavadoras-y-secadoras/c93'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 32,
+			'url' => 'http://www.bestbuy.com.mx/c/lavadoras-y-secadoras/c93'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 48,
+			'url' => 'http://www.bestbuy.com.mx/c/lentes-de-camara/c68'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 51,
+			'url' => 'http://www.bestbuy.com.mx/c/laptops/c41'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 33,
+			'url' => 'http://www.bestbuy.com.mx/c/licuadoras-y-batidoras/c251'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 34,
+			'url' => 'http://www.bestbuy.com.mx/c/microondas-y-tostadores/c94'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 54,
+			'url' => 'http://www.bestbuy.com.mx/c/barras-de-sonido/c1187'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 42,
+			'url' => 'http://www.bestbuy.com.mx/c/monitores-y-proyectores/c46'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 50,
+			'url' => 'http://www.bestbuy.com.mx/c/telefonos-celulares/c53'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 47,
+			'url' => 'http://www.bestbuy.com.mx/c/smartwatches/c206'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 46,
+			'url' => 'http://www.bestbuy.com.mx/c/tablets/c51'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 30,
+			'url' => 'http://www.bestbuy.com.mx/c/pantallas/c35'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 217,
+			'url' => 'http://www.bestbuy.com.mx/c/productos/c3?cat=10'
+		],
+		[
+			'tienda' => 1,
+			'categoria' => 218,
+			'url' => 'http://www.bestbuy.com.mx/c/all-in-one/c42'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 40,
+			'url' => 'http://wwvv.claroshop.com/categoria/16016/videocamaras/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 35,
+			'url' => 'http://wwvv.claroshop.com/categoria/16006/audifonos/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 36,
+			'url' => 'http://wwvv.claroshop.com/categoria/16007/bocinas--bafles--subwoofers/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 52,
+			'url' => 'http://wwvv.claroshop.com/categoria/16009/audio-portatil/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 53,
+			'url' => 'http://wwvv.claroshop.com/categoria/16009/audio-portatil/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 54,
+			'url' => 'http://wwvv.claroshop.com/categoria/16008/home-theater--amplificadores/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 37,
+			'url' => 'http://wwvv.claroshop.com/categoria/16008/home-theater--amplificadores/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 39,
+			'url' => 'http://wwvv.claroshop.com/categoria/16015/camaras/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 81,
+			'url' => 'http://wwvv.claroshop.com/categoria/16015/camaras/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 68,
+			'url' => 'http://wwvv.claroshop.com/categoria/16017/reflex--semiprofesionales--profesionales/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 58,
+			'url' => 'http://wwvv.claroshop.com/categoria/16037/disco-duro--usb-memoria/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 29,
+			'url' => 'http://wwvv.claroshop.com/categoria/16197/lavadoras-y-secadoras/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 32,
+			'url' => 'http://wwvv.claroshop.com/categoria/16197/lavadoras-y-secadoras/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 48,
+			'url' => 'http://wwvv.claroshop.com/categoria/16022/accesorios/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 51,
+			'url' => 'http://wwvv.claroshop.com/categoria/16034/laptops/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 33,
+			'url' => 'http://wwvv.claroshop.com/categoria/16216/licuadoras-y-picadoras/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 34,
+			'url' => 'https://w3.claroshop.com/categoria/16206/hornos-de-microondas/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 42,
+			'url' => 'http://wwvv.claroshop.com/categoria/16039/accesorios/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 50,
+			'url' => 'http://wwvv.claroshop.com/categoria/16002/celulares/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 47,
+			'url' => 'http://wwvv.claroshop.com/categoria/17900/smartwatch/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 46,
+			'url' => 'http://wwvv.claroshop.com/categoria/16035/tablets/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 30,
+			'url' => 'http://wwvv.claroshop.com/categoria/16023/pantallas/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 217,
+			'url' => 'http://wwvv.claroshop.com/categoria/16048/videojuegos/'
+		],
+		[
+			'tienda' => 2,
+			'categoria' => 218,
+			'url' => 'http://wwvv.claroshop.com/categoria/16033/desktops/'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 40,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/fotografia'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 39,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/fotografia'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 81,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/fotografia'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 68,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/fotografia'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 48,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/fotografia'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 35,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/audio/audifonos'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 36,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/audio/bocinas-amplificadores-y-receptores'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 52,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/audio/audio-portatil'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 53,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/audio'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 54,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/audio/barras-de-sonido'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 37,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/audio'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 58,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/computacion/discos-duros-y-memorias'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 51,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/computacion/laptops'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 51,
+			'url' => 'http://www.costco.com.mx/view/c/muebles-y-hogar/electrodomesticos/licuadoras-y-extractores-de-jugo'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 34,
+			'url' => 'http://www.costco.com.mx/view/c/linea-blanca/enseres-menores/microondas'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 42,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/tv-y-video/proyectores-y-pantallas'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 32,
+			'url' => 'http://www.costco.com.mx/view/c/linea-blanca/linea-blanca-lavanderia/secadoras'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 50,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/comunicacion/celulares-y-accesorios'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 46,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/computacion/tablets'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 30,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/tv-y-video?q=%3Arelevance'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 217,
+			'url' => 'http://www.costco.com.mx/view/c/electronica/consolas-y-videojuegos'
+		],
+		[
+			'tienda' => 3,
+			'categoria' => 218,
+			'url' => 'http://www.costco.com.mx/view/c/tecnologia-electronica-computo/computacion/desktops'
+		],
+		
+		
+		
+	
 	];
 
 	echo json_encode($paginas);
